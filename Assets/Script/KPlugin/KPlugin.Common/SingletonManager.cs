7affcfcf1fb2eeffe2e3f7c0d4ea23f1a6fbe24f
Kananat Suwanviwatana <kananat.s@gmail.com>
2017-01-09 17:38:44 +0900

    add KPlugin.Common add Singleton and Handler

---
+++ Assets/Script/KPlugin/KPlugin.Common/SingletonManager.cs
@@ -0,0 +1,17 @@
+﻿namespace KPlugin.Common
+{
+    using System.Linq;
+    using System.Reflection;
+    using UnityEngine;
+    using Editor;
+    using Extension;
+
+    public class SingletonManager : MonoBehaviour
+    {
+        [SerializedMethod]
+        public void Refresh()
+        {
+            Assembly.GetExecutingAssembly().GetTypes().Where(x => x.IsGenericSubclass(typeof(SingletonMonoBehaviour<>))).ToList().ForEach(x => this.GetOrAddComponent(x));
+        }
+    }
+}
---
+++ Assets/Script/KPlugin/KPlugin.Common/SingletonMonoBehaviour.cs
@@ -0,0 +1,61 @@
+﻿namespace KPlugin.Common
+{
+    using UnityEngine;
+
+    public class SingletonMonoBehaviour<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        private static bool applicationIsQuitting = false;
+        private static object _lock = new object();
+
+        private static T _instance;
+
+        public static T instance
+        {
+            get
+            {
+                if (applicationIsQuitting)
+                {
+                    Debug.LogWarning("[Singleton] Instance '" + typeof(T) + "' already destroyed on application quit." + " Won't create again - returning null.");
+                    return null;
+                }
+
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = (T)FindObjectOfType(typeof(T));
+
+                        if (FindObjectsOfType(typeof(T)).Length > 1)
+                        {
+                            Debug.LogError("[Singleton] Something went really wrong - there should never be more than 1 singleton! Reopening the scene might fix it.");
+                            return _instance;
+                        }
+
+                        if (_instance == null)
+                        {
+                            GameObject singleton = new GameObject();
+                            _instance = singleton.AddComponent<T>();
+                            singleton.name = "(singleton) " + typeof(T).ToString();
+
+                            DontDestroyOnLoad(singleton);
+
+                            Debug.Log("[Singleton] An instance of " + typeof(T) + " is needed in the scene, so '" + singleton + "' was created with DontDestroyOnLoad.");
+                        }
+                        else
+                        {
+                            Debug.Log("[Singleton] Using instance already created: " + _instance.gameObject.name);
+                        }
+                    }
+
+                    return _instance;
+                }
+            }
+        }
+
+
+        public void OnDestroy()
+        {
+            applicationIsQuitting = true;
+        }
+    }
+}
