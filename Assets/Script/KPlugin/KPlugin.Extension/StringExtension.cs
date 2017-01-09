36aecd199abd33a700148ef509b916a990aded6e
Kananat Suwanviwatana <kananat.s@gmail.com>
2017-01-09 13:18:19 +0900

    add KPlugin.Extension add StringExtension

---
+++ Assets/Script/KPlugin/KPlugin.Extension/StringExtension.cs
@@ -0,0 +1,23 @@
+﻿namespace KPlugin.Extension
+{
+    using System.Text.RegularExpressions;
+
+    public static class StringExtension
+    {
+        public static string ToCapital(this string s)
+        {
+            if (s == null)
+                return null;
+
+            if (s.Length > 1)
+                return char.ToUpper(s[0]) + s.Substring(1);
+
+            return s.ToUpper();
+        }
+
+        public static string ToRegular(this string s)
+        {
+            return Regex.Replace(s, "[a-z][A-Z]", x => x.Value[0] + " " + x.Value[1]).ToCapital();
+        }
+    }
+}
