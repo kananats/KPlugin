3bb38a24dee6d53e1bfdb2c3c8669d7b8b3b39cc
Kananat Suwanviwatana <kananat.s@gmail.com>
2017-01-15 23:29:41 +0900

    add RegexConstant

--- Assets/Script/KPlugin/KPlugin.Constant/System.Text.RegularExpressions/RegexConstant.cs
+++ Assets/Script/KPlugin/KPlugin.Constant/System.Text.RegularExpressions/RegexConstant.cs
@@ -4,14 +4,37 @@
 
     public static class RegexConstant
     {
-        private static Regex _alphanumeric;
+        private static Regex _numeric;
+        public static Regex numeric
+        {
+            get
+            {
+                if (_numeric == null)
+                    _numeric = new Regex("^[0-9]+$");
 
+                return _numeric;
+            }
+        }
+
+        private static Regex _alphabet;
+        public static Regex alphabet
+        {
+            get
+            {
+                if (_alphabet == null)
+                    _alphabet = new Regex("^[a-zA-Z]+$");
+
+                return _alphabet;
+            }
+        }
+
+        private static Regex _alphanumeric;
         public static Regex alphanumeric
         {
             get
             {
                 if (_alphanumeric == null)
-                    _alphanumeric = new Regex("^[a-zA-Z0-9]*$");
+                    _alphanumeric = new Regex("^[a-zA-Z0-9]+$");
 
                 return _alphanumeric;
             }
