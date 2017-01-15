cff61fd10cac880e2fecb56c055460fca23f3027
Kananat Suwanviwatana <kananat.s@gmail.com>
2017-01-15 23:30:44 +0900

    add support for -id -name

--- Assets/Script/KPlugin/KPlugin.Debug/Console.cs
+++ Assets/Script/KPlugin/KPlugin.Debug/Console.cs
@@ -8,6 +8,7 @@
     using UnityEngine;
     using UnityEngine.UI;
     using Extension;
+    using Constant;
 
     public class Console : MonoBehaviour
     {
@@ -21,79 +22,86 @@
             dictionary = new Dictionary<string, List<MethodInfo>>();
 
             Assembly.GetExecutingAssembly().GetTypes().Where(x => x.IsAbstract && x.IsSealed || !x.IsAbstract && !x.IsInterface && !x.IsGenericType && typeof(MonoBehaviour).IsAssignableFrom(x)).ToList().ForEach(x =>
-             {
-                 x.GetMethods(ConsoleMethodAttribute.bindingFlags).Where(y => !y.IsAbstract && !y.IsGenericMethod && !y.IsDefined<ExtensionAttribute>()).ToList().ForEach(y =>
-                 {
-                     ConsoleMethodAttribute attribute = y.GetCustomAttribute<ConsoleMethodAttribute>();
-                     if (attribute == null || attribute.name == null)
-                         return;
+            {
+                x.GetMethods(ConsoleMethodAttribute.bindingFlags).Where(y => !y.IsAbstract && !y.IsGenericMethod && !y.IsDefined<ExtensionAttribute>()).ToList().ForEach(y =>
+                {
+                    ConsoleMethodAttribute attribute = y.GetCustomAttribute<ConsoleMethodAttribute>();
+                    if (attribute == null || attribute.name == null)
+                        return;
 
-                     ParameterInfo[] parameterInfos = y.GetParameters();
-                     if (parameterInfos.Length >= 1 && parameterInfos[parameterInfos.Length - 1].IsDefined<ParamArrayAttribute>() || parameterInfos.ToList().Any(z =>
-                     {
-                         Type type = z.ParameterType;
-                         return z.IsOut || type.IsByRef || !type.IsPrimitive && type != typeof(string);
-                     }))
-                         return;
+                    ParameterInfo[] parameterInfos = y.GetParameters();
+                    if (parameterInfos.Length >= 1 && parameterInfos[parameterInfos.Length - 1].IsDefined<ParamArrayAttribute>() || parameterInfos.ToList().Any(z =>
+                    {
+                        Type type = z.ParameterType;
+                        return z.IsOut || type.IsByRef || !type.IsPrimitive && type != typeof(string);
+                    }))
+                        return;
 
-                     if (!dictionary.ContainsKey(attribute.name))
-                         dictionary[attribute.name] = new List<MethodInfo>();
+                    if (!dictionary.ContainsKey(attribute.name))
+                        dictionary[attribute.name] = new List<MethodInfo>();
 
-                     List<MethodInfo> list = dictionary[attribute.name];
-                     list.Add(y);
-                 });
-             });
+                    List<MethodInfo> list = dictionary[attribute.name];
+                    list.Add(y);
+                });
+            });
 
             inputField.onEndEdit.AddListener(x =>
             {
                 if (!Input.GetKeyDown(KeyCode.Return) && !Input.GetKeyDown(KeyCode.KeypadEnter))
                     return;
 
-                IEnumerable<string> tokens = x.SplitByWhiteSpace();
-                string name = tokens.First();
+                string name;
+                object[] arguments;
+                List<int> targetIdList;
+                List<string> targetIdName;
+
+                try
+                {
+                    Parse(x, out name, out arguments, out targetIdList, out targetIdName);
+                }
+                catch (Exception)
+                {
+                    Debug.Log(ConsoleMethodAttribute.unexpectedTokenError);
+                    ClearInputField();
+
+                    return;
+                }
 
                 if (!dictionary.ContainsKey(name))
                 {
                     Debug.Log(string.Format(ConsoleMethodAttribute.commandNotFoundError, name));
+                    ClearInputField();
+
                     return;
                 }
 
                 List<MethodInfo> list = dictionary[name];
 
-                object[] parameters = tokens.Skip(1).Select(y =>
-                {
-                    bool boolOutput;
-                    int intOutput;
-                    float floatOutput;
-
-                    return bool.TryParse(y, out boolOutput) ? boolOutput : (int.TryParse(y, out intOutput) ? intOutput : (float.TryParse(y, out floatOutput) ? floatOutput : (object)y));
-                }).ToArray();
-
                 MethodInfo mostCompatibleMethod = null;
-                object[] mostCompatibleConvertedParameters = null;
+                object[] mostCompatibleConvertedArguments = null;
                 int minCompatibility = int.MaxValue;
 
                 foreach (MethodInfo methodInfo in list)
                 {
-                    ParameterInfo[] targetParameters = methodInfo.GetParameters();
-                    if (parameters.Length != targetParameters.Length)
+                    ParameterInfo[] targetArguments = methodInfo.GetParameters();
+                    if (arguments.Length != targetArguments.Length)
                         continue;
 
                     int compatibility = 0;
-                    object[] convertedParameters = new object[parameters.Length];
+                    object[] convertedArguments = new object[arguments.Length];
 
-                    for (int i = 0; i < parameters.Length; i++)
+                    for (int i = 0; i < arguments.Length; i++)
                     {
-                        int parameterCompatibility;
-                        convertedParameters[i] = parameters[i].ChangeTypeWithCompatibility(targetParameters[i].ParameterType, out parameterCompatibility);
+                        int argumentCompatibility;
+                        convertedArguments[i] = arguments[i].ChangeTypeWithCompatibility(targetArguments[i].ParameterType, out argumentCompatibility);
 
-                        if (parameterCompatibility < 0)
+                        if (argumentCompatibility < 0)
                         {
                             compatibility = -1;
                             break;
                         }
 
-                        compatibility += parameterCompatibility;
+                        compatibility += argumentCompatibility;
                     }
 
                     if (compatibility < 0)
@@ -102,7 +110,7 @@
                     if (compatibility < minCompatibility)
                     {
                         mostCompatibleMethod = methodInfo;
-                        mostCompatibleConvertedParameters = convertedParameters;
+                        mostCompatibleConvertedArguments = convertedArguments;
                         minCompatibility = compatibility;
                     }
 
@@ -113,12 +121,14 @@
                 if (mostCompatibleMethod == null)
                 {
                     Debug.Log(string.Format(ConsoleMethodAttribute.argumentMismatchError, name));
+                    ClearInputField();
+
                     return;
                 }
 
                 try
                 {
-                    mostCompatibleMethod.AutoInvoke(mostCompatibleConvertedParameters);
+                    mostCompatibleMethod.AutoInvoke(y => targetIdList.Any(z => z == y.GetInstanceID()) || targetIdName.Any(z => z == y.name), mostCompatibleConvertedArguments);
                 }
                 catch (Exception)
                 {
@@ -127,10 +137,93 @@
                 }
                 finally
                 {
-                    inputField.ActivateInputField();
-                    inputField.text = "";
+                    ClearInputField();
                 }
             });
+        }
+
+        private void ClearInputField()
+        {
+            inputField.ActivateInputField();
+            inputField.text = "";
+        }
+
+        private static void Parse(string s, out string name, out object[] arguments, out List<int> targetIdList, out List<string> targetIdName)
+        {
+            Dictionary<string, List<object>> optionDictionary;
+
+            Parse(s, out name, out arguments, out optionDictionary);
+
+            targetIdList = optionDictionary.ContainsKey("id") ? optionDictionary["id"].Select(x => (int)x).ToList() : new List<int>();
+            targetIdName = optionDictionary.ContainsKey("name") ? optionDictionary["name"].Select(x => (string)x).ToList() : new List<string>();
+        }
+
+        private static void Parse(string s, out string name, out object[] arguments, out Dictionary<string, List<object>> optionDictionary)
+        {
+            string nameTemp = null;
+            List<object> argumentListTemp = null;
+            Dictionary<string, List<object>> optionDictionaryTemp = new Dictionary<string, List<object>>();
+
+            string lastOption = null;
+
+            s.SplitByWhiteSpace().ToList().ForEach(x =>
+            {
+                if (nameTemp == null)
+                {
+                    if (!x.IsMatch(RegexConstant.alphabet))
+                        throw new NotSupportedException();
+
+                    nameTemp = x;
+                    return;
+                }
+
+                if (x[0] == '-' && x.Substring(1).IsMatch(RegexConstant.alphabet))
+                {
+                    lastOption = x.Substring(1);
+
+                    if (lastOption == "args")
+                    {
+                        if (argumentListTemp != null)
+                            throw new NotSupportedException();
+
+                        argumentListTemp = new List<object>();
+                        return;
+                    }
+
+                    if (optionDictionaryTemp.ContainsKey(lastOption))
+                        throw new NotSupportedException();
+
+                    optionDictionaryTemp[lastOption] = new List<object>();
+                    return;
+                }
+
+                if (lastOption == null)
+                {
+                    lastOption = "args";
+                    argumentListTemp = new List<object>();
+                }
+
+                if (lastOption == "args")
+                {
+                    argumentListTemp.Add(Box(x));
+                    return;
+                }
+
+                optionDictionaryTemp[lastOption].Add(Box(x));
+            });
+
+            name = nameTemp;
+            arguments = argumentListTemp.ToArray();
+            optionDictionary = optionDictionaryTemp;
+        }
+
+        private static object Box(string s)
+        {
+            bool boolOutput;
+            int intOutput;
+            float floatOutput;
+
+            return bool.TryParse(s, out boolOutput) ? boolOutput : (int.TryParse(s, out intOutput) ? intOutput : (float.TryParse(s, out floatOutput) ? floatOutput : (object)s));
         }
     }
 }
