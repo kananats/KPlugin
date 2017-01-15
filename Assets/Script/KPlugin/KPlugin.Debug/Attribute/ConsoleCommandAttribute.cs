559d33567c35c47bd8f44762db11afadc39013bf
Kananat Suwanviwatana <kananat.s@gmail.com>
2017-01-15 17:00:48 +0900

    improve Log string

--- Assets/Script/KPlugin/KPlugin.Debug/Attribute/ConsoleCommandAttribute.cs
+++ Assets/Script/KPlugin/KPlugin.Debug/Attribute/ConsoleCommandAttribute.cs
@@ -11,7 +11,7 @@
         public static BindingFlags bindingFlags = BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic;
 
         public static string commandNotFoundError = "'{0}' is not recognized as a command";
-        public static string argumentMismatchError = "No overload for '{0}' which takes {1} argument(s)";
+        public static string argumentMismatchError = "argument mismatch for invoking '{0}'";
         public static string runtimeError = "unexpected error while running '{0}'";
 
         public string name
