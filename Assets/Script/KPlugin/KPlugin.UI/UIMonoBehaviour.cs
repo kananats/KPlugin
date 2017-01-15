d92dd7b8a1a1cc1f0b247d9fe5f469841d8832ab
Kananat Suwanviwatana <kananat.s@gmail.com>
2017-01-15 11:24:49 +0900

    KPlugin.UI add UIMonoBehaviour

---
+++ Assets/Script/KPlugin/KPlugin.UI/UIMonoBehaviour.cs
@@ -0,0 +1,21 @@
+﻿namespace KPlugin.UI
+{
+    using UnityEngine;
+
+    [RequireComponent(typeof(RectTransform))]
+    public class UIMonoBehaviour : MonoBehaviour
+    {
+        private RectTransform _rectTransform;
+
+        public RectTransform rectTransform
+        {
+            get
+            {
+                if (_rectTransform == null)
+                    _rectTransform = transform as RectTransform;
+
+                return _rectTransform;
+            }
+        }
+    }
+}
