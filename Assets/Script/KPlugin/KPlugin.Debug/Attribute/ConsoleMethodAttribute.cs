67cb8acb192d4b16fdb3dd14c32c58d9d5e63c45
Kananat Suwanviwatana <kananat.s@gmail.com>
2017-01-16 23:45:36 +0900

    add and optimize error message

--- Assets/Script/KPlugin/KPlugin.Debug/Attribute/ConsoleMethodAttribute.cs
+++ Assets/Script/KPlugin/KPlugin.Debug/Attribute/ConsoleMethodAttribute.cs
@@ -2,18 +2,20 @@
 {
     using System;
     using System.Reflection;
-    using Extension;
-    using Constant;
 
     [AttributeUsage(AttributeTargets.Method)]
     public class ConsoleMethodAttribute : Attribute
     {
-        public static BindingFlags bindingFlags = BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic;
+        public static readonly BindingFlags bindingFlags = BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic;
 
-        public static string unexpectedTokenError = "unexpected token";
-        public static string commandNotFoundError = "'{0}' is not recognized as a command";
-        public static string argumentMismatchError = "argument mismatch for invoking '{0}'";
-        public static string runtimeError = "unexpected error while running '{0}'";
+        public static readonly string unsupportedCommandNameError = "'{0}' contains unsupported character(s)";
+        public static readonly string reservedKeywordError = "'{0}' is used as reserved keyword";
+        public static readonly string unsupportedArgumentError = "'{0}' contains unsupported argument type(s)";
+        public static readonly string duplicatedCommandError = "'{0}' is already defined with same argument type(s)";
+        public static readonly string unexpectedTokenError = "'{0}' is not a valid input string";
+        public static readonly string commandNotFoundError = "'{0}' is not recognized as a command";
+        public static readonly string mismatchArgumentError = "Argument mismatch for invoking '{0}'";
+        public static readonly string runtimeError = "Unexpected error while running '{0}'";
 
         public string name
         {
@@ -23,7 +25,7 @@
 
         public ConsoleMethodAttribute(string name)
         {
-            this.name = name.IsMatch(RegexConstant.alphabet) ? name : null;
+            this.name = name;
         }
     }
 }
