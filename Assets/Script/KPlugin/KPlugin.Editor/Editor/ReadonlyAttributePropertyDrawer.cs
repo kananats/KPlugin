ae192b829507aa51e84f66134b9a049d1b98c90e
Kananat Suwanviwatana <kananat.s@gmail.com>
2017-01-26 22:43:31 +0900

    refactor

--- Assets/Script/KPlugin/KPlugin.Editor/Editor/ReadonlyAttributePropertyDrawer.cs
+++ Assets/Script/KPlugin/KPlugin.Editor/Editor/ReadonlyAttributePropertyDrawer.cs
@@ -6,12 +6,12 @@
     [CustomPropertyDrawer(typeof(ReadonlyAttribute))]
     public class ReadonlyAttributePropertyDrawer : PropertyDrawer
     {
-        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        override public float GetPropertyHeight(SerializedProperty property, GUIContent label)
         {
             return EditorGUI.GetPropertyHeight(property, label, true);
         }
 
-        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        override public void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
             GUI.enabled = false;
             EditorGUI.PropertyField(position, property, label, true);
